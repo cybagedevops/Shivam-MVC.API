18b33e81c84c7d4042b1820f757bfb8d9626ecc1
Shivam <shivamja@cybage.com>
2021-12-14 14:37:38 +0530

    api code uploaded

---
+++ MVC.API/MVC.API/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MVC.API.Models;
+namespace MVC.API.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        WebAppDbEntities7 db = new WebAppDbEntities7();
+
+        public IEnumerable<Employee> Get()
+        {
+          
+            return db.Employees.ToList();
+        }
+           
+    }
+}
